7575d472b3c8de6fb080527e583516c64ce3507e
khaywar <kdhaywar@ualberta.ca>
2016-02-14 14:36:13 -0700

    added template dir for posts index.html and some tutorial functionality

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    
+    ##### snagged from django tut1.8, using as example
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    ###################################################
+
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
-
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_post_list = Post.objects.order_by('pub_date')[:5]
+    output = ', '.join([p.post_text for p in latest_post_list])
+    return HttpResponse(output)
+
+
+######Copied from django tut1.8, using them as examples
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+########################################################
