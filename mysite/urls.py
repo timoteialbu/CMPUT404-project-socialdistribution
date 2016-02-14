bdb06d99619a5dd20e20707bf2cb294bda6e0edd
khaywar <kdhaywar@ualberta.ca>
2016-02-14 16:58:03 -0700

    complete up to end of part 3

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,14 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from mysite.views import Index
 
-admin.autodiscover()
+admin.autodiscover()  ##according to the django tut (1.8) this shouldnt be here? idk
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', Index.as_view(), name='index'),
-	url(r'^posts/', include('posts.urls')),
+                       url(r'^posts/', include('posts.urls', namespace="posts")),
     url(r'^admin/', include(admin.site.urls)),
 )
 
