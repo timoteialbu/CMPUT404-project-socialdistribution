90c5c1bc2c93bc569bd7b811f2058193ba2e0587
khaywar <kdhaywar@ualberta.ca>
2016-02-14 22:51:23 -0700

    can add new posts to database, janky though

--- posts/urls.py
+++ posts/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     # ex: /polls/5/
     url(r'^(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+#    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+#    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     ###################################################
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse ### delete me! should be using render
-from .models import Post
+from django.utils import timezone
 
+from .models import Post
+from .forms import PostForm
 
 def index(request):
     latest_post_list = Post.objects.order_by('pub_date')[:5]
@@ -11,19 +13,31 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 def createPost(request):
-    return render(request, 'posts/createPost.html')
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.pub_date = timezone.now()
+            post.save()
+            return redirect('detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'posts/createPost.html', {'form': form})
 
 def publish(request):
     return HttpResponse(request)
+
+
 ######Copied from django tut1.8, using them as examples
 def detail(request, post_id):
     post = get_object_or_404(Post, pk = post_id)
     return render(request, 'posts/detail.html', {'post': post})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 ########################################################
