9ab4a2130357e7412da0630c4ded17cc1f3a5f50
Kyle <kdhaywar@ualberta.ca>
2016-03-03 23:28:37 -0700

    working on friend requests, cut down friend mgmt code in view

--- posts/forms.py
+++ posts/forms.py
@@ -30,3 +30,35 @@ class AddFriendForm(forms.Form):
 class UnFriendUserForm(forms.Form):
     username = forms.CharField(label='username', required=False)
 
+
+class FriendRequestForm(forms.Form):
+    users = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        label="Notify and subscribe users to this post:")
+
+
+
+# class DivisionRemovalForm(forms.Form):
+#     class MultipleDivisionField(forms.ModelMultipleChoiceField):
+#         def label_from_instance(self, obj):
+#             url = reverse('organizer_division_view', kwargs={'div_pk': obj.pk})
+#             label = '<a href="%s">%s</a>' % (url, obj.__unicode__())
+#             return mark_safe(label)
+
+#     divisions = MultipleDivisionField(queryset=[],
+#                 widget=forms.CheckboxSelectMultiple())
+
+#     def __init__(self, competition, *args, **kwargs):
+#         self.competition = competition
+#         super(DivisionRemovalForm, self).__init__(*args, **kwargs)
+#         self.fields['divisions'].queryset = self.competition.divisions.all()
+
+#     def save(self):
+#         for div in self.cleaned_data['divisions']:
+#             units = div.units.all()
+#             if units:
+#                 for unit in units:
+#                     unit.in_division = False
+#                     unit.save()
+#         divisions = self.cleaned_data['divisions']
+#         divisions.delete()
