8959ae3552a6c445e57edf7df997986a9d4868aa
Yufei Zhang <yufeiGitHub@gmail.com>
2016-03-09 18:33:40 -0700

    I added delete_post(request) and change accounts/login to account/login.
    But I still don’t know why Upload Image, Manage Posts and Manage
    Friends do not work.

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     url(r'^$', Index.as_view(), name='index'),
     url(r'^posts/', include('posts.urls', namespace="posts")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^account/', include('allauth.urls')),
     url(r'^api/', include('api.urls')),
 )
 
