21dccd6e4d570b8461df3482ae3fce6b492a6f39
Alain Clark <aac.clark@gmail.com>
2016-03-11 19:16:19 -0700

    JSON Tests probably

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
 
 
 ######CHANGE!!! Run my_setup.py
-MEDIA_ROOT = '/Users/Yufei/Desktop/CMPUT404-project-socialdistribution/media/'
+MEDIA_ROOT = '/Users/alain/workspace/csvm/~04/project/CMPUT404-project-socialdistribution/media/'
 MEDIA_URL = '/media/'
 
 
