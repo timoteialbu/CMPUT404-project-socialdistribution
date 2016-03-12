36b6cc1ceba7a7ddd93a7ff9790e023f2298cf5c
Timotei Albu <zztimy@users.noreply.github.com>
2016-03-12 01:02:03 -0700

    change media root

--- mysite/settings.py
+++ mysite/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
 
 
 ######CHANGE!!! Run my_setup.py
-MEDIA_ROOT = '/home/kyle/Desktop/404/CMPUT404-project-socialdistribution/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
 
