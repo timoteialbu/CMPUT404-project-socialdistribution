acfecab823a8f7f20997e326af8b84db0d0a398a
shawn <swanders@ualberta.ca>
2016-03-20 17:19:41 -0600

    PAGINATION WORKING

--- mysite/settings.py
+++ mysite/settings.py
@@ -159,8 +159,7 @@ REST_FRAMEWORK = {
     #'PAGE_SIZE': 100
     # 'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination'
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination'
 }
 
 
