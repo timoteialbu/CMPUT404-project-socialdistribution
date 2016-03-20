5655f708e7565ab0f13898bb4b19eef065335cda
Kyle <kdhaywar@ualberta.ca>
2016-03-19 18:34:34 -0600

    posts serializor portion of the api is almost complete, comment next doesnt work

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from api.models import Post, Author, Comment
-from django.core.paginator import Paginator
 from rest_framework import pagination
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,13 +12,16 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Comment
         fields = ('author', 'comment', 'contentType', 'published', 'identity',)
 
+
 class UserSerializer(serializers.ModelSerializer):
-    #userinfo = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    # userinfo = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
     author = AuthorSerializer(source='author')
+
     class Meta:
         model = User
         fields = ('author')
@@ -38,37 +41,42 @@ class PostSerializer(serializers.ModelSerializer):
     # url and github (see api protocols)
     author = AuthorSerializer(read_only=True)
     # comment = CommentSerializer(many=True, read_only=False)
-    comment = serializers.SerializerMethodField('paginated_comments')
+    comments = serializers.SerializerMethodField('paginated_comments')
     # comment = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    count = serializers.SerializerMethodField('comment_count')
+    # next = serializers.SerializerMethodField('next_page')
+    size = serializers.SerializerMethodField('comment_size')
+    next = serializers.SerializerMethodField('next_page')
 
     class Meta:
         model = Post
         fields = (
             'title', 'source', 'origin', 'description',
             'contentType', 'content', 'author', 'categories',
-            'comment', 'published', 'identity', 'visibility',
+            'count', 'size', 'next',
+            'comments', 'published', 'identity', 'visibility',
         )
 
     def paginated_comments(self, obj):
-        comments = Comment.objects.filter(post=obj)[:5]
+        comments = Comment.objects.filter(post=obj)  # [:5]
         paginator = pagination.PageNumberPagination()
         page = paginator.paginate_queryset(comments, self.context['request'])
-        serializer = CommentSerializer(page, many=True, context={'request': self.context['request']})
+        serializer = CommentSerializer(
+            page,
+            many=True,
+            context={'request': self.context['request']}
+        )
         return serializer.data
 
+    def next_page(self, obj):
+        print "not complete"
+        # if not self.Paginated_comments.has_next():
+        #    return None
+        # page_number = self.page.next_page_number()
+        # return replace_query_param('', self.page_query_param, page_number)
 
+    def comment_count(self, obj):
+        return len(Comment.objects.filter(post=obj))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def comment_size(self, obj):
+        return 5
