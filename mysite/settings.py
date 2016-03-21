45be54400c00c83fe30b40cb58b1f544b57f2cf8
zztimy <timotei@ualberta.ca>
2016-03-21 14:33:00 -0600

    Revert "removed jwt setttings stuff"
    This reverts commit 3bc68347bfb8324e5c1f4d5643f17ffd2f3bce7b.

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,12 +16,9 @@ else:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # adjust to turn off when on Openshift, but allow an environment variable to override on PAAS
-<<<<<<< HEAD
-#DEBUG = ON_PAAS
-=======
 
->>>>>>> kdhaywar/master
 DEBUG = not ON_PAAS
+
 DEBUG = DEBUG or os.getenv("debug","false").lower() == "true"
 
 if ON_PAAS and DEBUG:
@@ -158,7 +155,36 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination'
 }
 
+JWT_AUTH = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
 
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+    'JWT_SECRET_KEY': settings.SECRET_KEY,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=30000),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+}
 
 
 
