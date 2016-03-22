f99ba6154483066bfc6153914c23e8cbdbedaad4
Alain Clark <aac.clark@gmail.com>
2016-03-21 21:14:30 -0600

    All displays inheriting from base.html are now responsive and have navbars. Signup form is fixed for all displays.

--- mysite/settings.py
+++ mysite/settings.py
@@ -235,8 +235,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #os.path.join(BASE_DIR,"static"),
-    os.path.join(os.path.dirname(__file__), 'static',)
+    os.path.join(BASE_DIR,"static"),
+    #os.path.join(os.path.dirname(__file__), 'static'),
 )
 
 TEMPLATE_DIRS = (
