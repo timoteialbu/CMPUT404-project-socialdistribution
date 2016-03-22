ed6f631753c5ea1ef99f9a399b84cb689c7aa3ec
Alain Clark <aac.clark@gmail.com>
2016-03-22 06:06:24 -0600

    You get a <center> and YOU get a <center>. EVERYBODY gets a <center>! SO RESPONSIVE!

--- posts/urls.py
+++ posts/urls.py
@@ -3,11 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^create_img/$', views.create_img, name='create_img'),
-
     url(r'^$', views.index, name='index'),
 
-    #url(r'^create_img/$', views.create_img, name='create_img'),
+    url(r'^create_img/$', views.create_img, name='create_img'),
+
     url(r'^post_mgnt/$', views.post_mgnt, name='post_mgnt'),
 
     url(r'^friend_mgnt/$', views.friend_mgnt, name='friend_mgnt'),
