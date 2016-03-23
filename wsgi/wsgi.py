42124b7ea8bdc681dcc155a8f3f0077a515e31eb
Timotei Albu <timotei@ualberta.ca>
2016-03-23 12:30:59 -0600

    Trying to get heroku to work

--- wsgi/wsgi.py
+++ wsgi/wsgi.py
@@ -1,14 +1,23 @@
-"""
-WSGI config for mysite project.
 
+"""
+WSGI config for socialDistribution project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
+
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+
+'''
+dj-static allows you to properly serve static assets 
+from production with a WSGI server: https://pypi.python.org/pypi/dj-static
+''' 
+from dj_static import Cling
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
